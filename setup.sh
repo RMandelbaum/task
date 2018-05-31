5b5dc0b6450e0134661c3bacea5c0fd363d9db61
Rachel <rlmandelbaum@gmail.com>
2018-05-31 09:41:28 -0400

    made set.sh executable

--- setup.sh
+++ setup.sh
@@ -3,10 +3,12 @@
 gem install bundler
 bundle install
 bundle exec rake db:create db:migrate db:seed
-
+echo "
 #To change category questions, the DB is set up that you can easily edit directly in the app/views
 #To add category questions, the DB is set up that you can easily add directly in the app/views
 
 
 #User must login to fill out forms
-#Either login as username: Rachel password: Password or click become a tasker and sign up 
+#Either login as username: Rachel password: Password or click become a tasker and sign up
+
+"
